cc68cb03c16e0bf566cf7b671f3c275d08ca6ea1
Sristi27 <sristi2705@gmail.com>
2021-05-22 22:52:06 +0530

    heroku

---
+++ config/keys.js
@@ -0,0 +1,7 @@
+module.exports={
+
+    SENDGRID_API:'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',
+    MONGO_URI:'mongodb+srv://sristi27:270520@cluster0.hwmrk.mongodb.net/user?retryWrites=true&w=majority',
+    STRIPE_ID:'sk_test_51IpczwSH2HRN2Fzs9KpJpmMH3VCqaAMVsHLn4g97qdCm3FFjNHKjnESbnWUuOegnC1FHCI6l9OxqY3fr5CBvVqXf007qCfTJBN'
+
+}
